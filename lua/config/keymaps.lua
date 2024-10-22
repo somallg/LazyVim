061a8309d0d73e90413796ff88fe38662e89691d
somallg <tqduong86@gmail.com>
2024-10-22 21:22:28 +0700

    add terminal module

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  { "i", "n" },
+  "<C-c><C-c>",
+  require("somallg.terminal").exec_code_block_at_cursor,
+  { desc = "Execute code at cursor" }
+)
