8923287dc62a7257c64e71c91349b17111926f5f
somallg <tqduong86@gmail.com>
2024-11-23 22:48:51 +0700

    disable render-markdown and conceallevel

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("somallg_" .. name, { clear = true })
+end
+
+-- Fix conceallevel for markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("markdown_conceal"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
