b4618fd3d873229c1b056f497fd45ca7f1ece383
somallg <tqduong86@gmail.com>
2024-11-24 18:08:01 +0700

    refactor(terminal): remove toggleterm

--- lua/somallg/terminal.lua
+++ lua/somallg/terminal.lua
@@ -20,9 +20,19 @@ local function get_code_block_at_cursor()
   return vim.treesitter.get_node_text(code_block_node, vim.fn.bufnr("%"))
 end
 
-function M.exec_code_block_at_cursor()
-  local toggleterm = require("toggleterm")
+local function send_to_terminal(cmd)
+  local terminal = Snacks.terminal.get(nil, { cwd = LazyVim.root(), win = { position = "right" } })
 
+  if terminal == nil then
+    print("No terminal found")
+    return
+  end
+
+  local job_ib = vim.b[terminal.buf].terminal_job_id
+  vim.fn.chansend(job_ib, cmd .. "\n")
+end
+
+function M.exec_code_block_at_cursor()
   local code_block = get_code_block_at_cursor()
 
   if code_block == nil then
@@ -30,7 +40,7 @@ function M.exec_code_block_at_cursor()
     return
   end
 
-  toggleterm.exec(code_block)
+  send_to_terminal(code_block)
 end
 
 return M
