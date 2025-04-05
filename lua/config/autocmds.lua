315d649dc2763bb42e0bec7cb8e47596ea80b08f
somallg <tqduong86@gmail.com>
2025-04-05 14:09:05 +0700

    disable markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,8 +7,21 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("somallg_" .. name, { clear = true })
+end
+
+-- Fix conceallevel for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("markdown_conceal"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  desc = "Reset cursor back to beam when leaving Neovim",
-  group = vim.api.nvim_create_augroup("somallg-reset-cursor-on-exit", { clear = true }),
+  group = augroup("reset_cursor_on_exit"),
   command = "set guicursor=a:ver25",
+  desc = "Reset cursor back to beam when leaving Neovim",
 })
