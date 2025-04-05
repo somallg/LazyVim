f07d5e292b4c9b0ff5635af5165ce0ebcabb15d0
somallg <tqduong86@gmail.com>
2025-04-05 14:05:39 +0700

    reset cursor on nvim exit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  desc = "Reset cursor back to beam when leaving Neovim",
+  group = vim.api.nvim_create_augroup("somallg-reset-cursor-on-exit", { clear = true }),
+  command = "set guicursor=a:ver25",
+})
