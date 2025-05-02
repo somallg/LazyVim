08d0750f588397815b0fbb46d4408de52f390405
somallg <tqduong86@gmail.com>
2025-05-02 21:58:13 +0700

    feat(markdown): disable render markdown and conceallevel

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("somallg_" .. name, { clear = true })
+end
+
+-- Fix conceallevel for markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("markdown_conceal"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
