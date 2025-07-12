667333f5cd2dcb11015c60a1a67520aab4b40cb9
somallg <tqduong86@gmail.com>
2025-07-12 16:56:27 +0700

    config snack files to show hidden files

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,11 +1,17 @@
 return {
   "folke/snacks.nvim",
+  ---@module "snacks"
   ---@type snacks.Config
   opts = {
     picker = {
       formatters = {
         file = {
           truncate = 120,
+        },
+      },
+      sources = {
+        files = {
+          hidden = true,
         },
       },
     },
