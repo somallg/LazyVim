98d3cd5044e963be64ec391bf383206a13110a48
somallg <tqduong86@gmail.com>
2025-08-07 16:35:19 +0700

    feat(ui): add bufferline.nvim config with always_show_bufferline and auto_toggle_bufferline set to false
    Signed-off-by: somallg <tqduong86@gmail.com>

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    ---@module "bufferline"
+    ---@type bufferline.UserConfig
+    opts = {
+      options = {
+        always_show_bufferline = false,
+        auto_toggle_bufferline = false,
+      },
+    },
+  },
+}
