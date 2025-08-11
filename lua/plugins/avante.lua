19383dddceec1f98e9493abd60bed6c867cd3dee
somallg <tqduong86@gmail.com>
2025-08-11 10:26:43 +0700

    feat: udpate avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,43 +1,24 @@
 return {
   "yetone/avante.nvim",
-  cmd = "AvanteAsk",
   build = "make",
+  event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
-  keys = {
-    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-  },
-  ---@module 'avante'
+  ---@module "avante"
   ---@type avante.Config
   opts = {
     input = {
       provider = "snacks",
-      provider_opts = {
-        title = "Avante Input",
-        icon = " ",
-      },
     },
     provider = "copilot",
-    providers = {
-      ---@type AvanteSupportedProvider
-      copilot = {
-        model = "gpt-4.1",
-      },
+    copilot = {
+      model = "gpt-4.0",
     },
     windows = {
       width = 50,
     },
   },
-  version = false, -- Never set this value to "*"! Never!
+  version = false,
 }
-
--- dependencies
--- keys
--- lazy = true,
--- opts
--- version
---
--- input
--- provider
