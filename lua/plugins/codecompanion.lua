bfe0b6f5ff43f89fb26e9c5e044e21e9784e161b
somallg <tqduong86@gmail.com>
2025-10-07 22:28:21 +0700

    feat(codecompanion): add notification hooks and update chat display configuration

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,8 +1,5 @@
-local devops_prompt = [[
-You are a Principal DevOps engineer working at a healthcare company.
-Your role is to provide scalable, efficient, and automated solutions for software deployment, infrastructure management, and CI/CD pipelines.
-Suggest the best DevOps practices, including infrastructure setup, deployment strategies, automation tools, and cost-effective scaling solutions.
-]]
+local devops_prompt =
+  "You are a Principal DevOps engineer working at a healthcare company. Your role is to provide scalable, efficient, and automated solutions for software deployment, infrastructure management, and CI/CD pipelines. Suggest the best DevOps practices, including infrastructure setup, deployment strategies, automation tools, and cost-effective scaling solutions."
 
 return {
   {
@@ -10,7 +7,47 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    keys = {
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      {
+        "<leader>aa",
+        function()
+          return require("codecompanion").toggle()
+        end,
+        desc = "Toggle (CodeCompanion)",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ap",
+        function()
+          require("codecompanion").actions({})
+        end,
+        desc = "Prompt Actions (CodeCompanion)",
+        mode = { "n", "v" },
+      },
+    },
+    init = function()
+      local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = "CodeCompanionRequestStarted",
+        group = group,
+        callback = function()
+          vim.notify("Planning response...", vim.log.levels.INFO, { title = "CodeCompanion" })
+        end,
+      })
+    end,
     opts = {
+      display = {
+        chat = {
+          window = {
+            opts = {
+              numberwidth = 4,
+            },
+            width = 0.5,
+          },
+        },
+      },
       strategies = {
         chat = {
           adapter = {
