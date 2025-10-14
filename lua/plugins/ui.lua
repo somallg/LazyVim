2497f5a32843cf7886e8f96fed67af86660ff0e9
somallg <tqduong86@gmail.com>
2025-10-14 11:13:55 +0700

    feat(ui): add and configure bufferline plugin

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    ---@module "bufferline"
+    ---@type bufferline.UserConfig
+    opts = {
+      options = {
+        auto_toggle_bufferline = false,
+      },
+    },
+  },
+}
