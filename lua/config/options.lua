a3a0f1e29dd4dd5014122f159587de7adc7bcbfe
somallg <tqduong86@gmail.com>
2025-10-14 11:25:41 +0700

    feat: disable animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable animations
+vim.g.snacks_animate = false
+
 -- Disable conceallevel
 vim.opt.conceallevel = 0
