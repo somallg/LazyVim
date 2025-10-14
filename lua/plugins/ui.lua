fdcdf299d26b7147bc66fe641485b6ab33648a9d
somallg <tqduong86@gmail.com>
2025-10-14 18:26:44 +0700

    feat: disable snacks dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,4 +9,14 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    ---@module "snacks"
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        enabled = false,
+      },
+    },
+  },
 }
