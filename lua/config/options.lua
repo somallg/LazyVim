6f21edd55f09ba50e661203105ff7f51b2673be0
somallg <tqduong86@gmail.com>
2025-10-16 12:34:01 +0700

    feat: disable auto format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable auto format
+vim.g.autoformat = false
+
 -- Disable animations
 vim.g.snacks_animate = false
 
